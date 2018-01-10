2222e5760f207c1862077e1d53da11f1ca89eda1
birdomi <ryk012@naver.com>
2018-01-10 13:35:18 +0900

    Update detector.py

--- detector.py
+++ detector.py
@@ -3,30 +3,43 @@ import numpy as np
 import a
 import scipy.io.wavfile as wav
 import time
+import random
 
+randomindex=random.sample(range(10),10)
+for i in range(10):
+    print(str(randomindex[i]))
 
 data=a.Data()
-data.Data_Append('Bdb001.interaction.wav','Bdb001.txt')
+data.Data_Append('Bed004.interaction.txt','Bed004_test.txt')
+data.Data_Load_forSmall_Label('Bed005.interaction.txt','Bed005_test.txt')
 data.Make_Batch()
 
 ###
 sess=tf.Session()
 rnn_model=a.RNN_model(sess,'rnn')
 sess.run(tf.global_variables_initializer())
+#rnn_model.restore()
 
- 
-for i in range(1001):
+print(data.train.data_length)
+for i in range(1,501):
     avg_cost=0
     avg_train1=0.0
     avg_train2=0.0
     avg_test1=0.0
     avg_test2=0.0
     start=time.time()
-    for j in range(data.train.data_length):
-        cost,out,_=rnn_model.train(data.train.mfcc_data[j],data.train.label_data[j],len(data.train.mfcc_data[j]))
+    
+    randomIndex=random.sample(range(data.train.data_length),data.train.data_length)
+    for j in range(data.train.data_length):        
+        cost,out,_=rnn_model.train(data.train.mfcc_data[randomIndex[j]],data.train.label_data[randomIndex[j]],len(data.train.mfcc_data[randomIndex[j]]))
         avg_cost+=cost
     traintime=time.time()-start
-    if(i%50==0):
+    """
+    if(i%100==0):
+        rnn_model.save()
+        print('model saved')
+    """
+    if(i%10==0):                
         for j in range(data.train.data_length):
             ac1,ac2=rnn_model.accuracy(data.train.mfcc_data[j],data.train.label_data[j])
             avg_train1+=ac1
@@ -41,6 +54,20 @@ for i in range(1001):
         avg_test2=avg_test2/data.test.data_length    
         print('accuracy_train1:',avg_train1,'accuracy_train2',avg_train2)
         print('accuracy_test1:',avg_test1,'accuracy_test2',avg_test2)
+
+    if(i%10==0):
+        print('test Small Label')
+        ac1_test_small_Label=0.0
+        ac2_test_small_Label=0.0
+        for j in range(data.test_small_label.data_length):
+            ac1,ac2=rnn_model.accuracy(data.test_small_label.mfcc_data[j],data.test_small_label.label_data[j])
+            ac1_test_small_Label+=ac1
+            ac2_test_small_Label+=ac2
+        ac1_test_small_Label=ac1_test_small_Label/data.test_small_label.data_length
+        ac2_test_small_Label=ac2_test_small_Label/data.test_small_label.data_length
+
+        print(ac1_test_small_Label)
+        print(ac2_test_small_Label)
     
     
     avg_cost=avg_cost/data.train.data_length
