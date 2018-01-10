7b310a8f2db40758621b5458af3fc55ac94ee6c3
birdomi <ryk012@naver.com>
2018-01-10 13:34:56 +0900

    Update a.py

--- a.py
+++ a.py
@@ -3,21 +3,18 @@ import tensorflow as tf
 import scipy.io.wavfile as wav
 import python_speech_features
 import winsound
-import sklearn.preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
 #config
-label_weight=2
 time_windowSteps=80#*0.01s
 time_windowLength=4*80#*0.01s
-learning_rate=1e-2
-batch_size=200
-test_rate=0.5
+learning_rate=0.002
+batch_size=150
+test_rate=0.7
 #
 
 vec_per_frame=39
 no_output=2
-seq_length=10*(2*label_weight+1)
-vec_per_sequence=vec_per_frame*seq_length
 beep_frequency=1500
 beep_duration=2000
 
@@ -36,23 +33,29 @@ class Data(object):
 
     train=mfcc_label_data()
     test=mfcc_label_data()
+    test_small_label=mfcc_label_data()
     
 
-    def Mfcc(self,s):
-        fs,audio=wav.read('Wave/'+s)
-        mfcc=python_speech_features.base.mfcc(audio,fs)
+    def Mfcc(self,s):        
+        mfcc=np.loadtxt('wave/'+s)
         delta=python_speech_features.base.delta(mfcc,1)
         ddelta=python_speech_features.base.delta(delta,1)
-
+        
         Data_length=(int)(len(mfcc)/time_windowSteps)
         tmp_data_for_X=[]
         
         seq=(int)(time_windowLength/time_windowSteps)
         num_seq=Data_length-seq
         for i in range(Data_length):
-            tmp_data_for_X.append(Data.Return_a_Sequence_data(mfcc,i))
-            tmp_data_for_X.append(Data.Return_a_Sequence_data(delta,i))
-            tmp_data_for_X.append(Data.Return_a_Sequence_data(ddelta,i))
+            tmp_data_for_X.append(Data.Return_a_Sequence_data(mfcc,i*time_windowSteps))
+            tmp_data_for_X.append(Data.Return_a_Sequence_data(delta,i*time_windowSteps))
+            tmp_data_for_X.append(Data.Return_a_Sequence_data(ddelta,i*time_windowSteps))
+        tmp_data_for_X=np.reshape(tmp_data_for_X,(-1,39))
+        """
+        scaler=MinMaxScaler()
+        scaler.fit(tmp_data_for_X)
+        tmp_data_for_X=scaler.transform(tmp_data_for_X)
+       """
         tmp_data_for_X=np.reshape(tmp_data_for_X,(-1,time_windowSteps,vec_per_frame))
         tmp_seq=[]
         for i in range(num_seq):
@@ -71,7 +74,7 @@ class Data(object):
 
         tmp_data_for_Y=[]
         for i in range(Data_length):
-            tmp_data_for_Y.append(Data.Return_a_Sequence_data(tmp,i))        
+            tmp_data_for_Y.append(Data.Return_a_Sequence_data(tmp,i*time_windowSteps))        
         tmp_data_for_Y=np.reshape(tmp_data_for_Y,(-1,time_windowSteps))
 
         dataY=[]
@@ -91,7 +94,7 @@ class Data(object):
         append_label=self.Labeling(labelpath)
         self.label_data=np.append(self.label_data,append_label)
         self.label_data=np.reshape(self.label_data,(-1,time_windowLength))
-        print(self.label_data.shape)
+        print(self.label_data.shape)        
 
     def Make_Batch(self):
         data_length=len(self.label_data)
@@ -128,6 +131,36 @@ class Data(object):
         temp=np.reshape(temp,(-1))
         return temp     
 
+    def Data_Load_forSmall_Label(self,mfccpath,labelpath):
+        append_mfcc=self.Mfcc(mfccpath)
+        mfcc_data=append_mfcc
+        mfcc_data=np.reshape(mfcc_data,(-1,time_windowLength,vec_per_frame))
+        print(mfcc_data.shape)
+
+        append_label=self.Labeling(labelpath)
+        label_data=append_label
+        label_data=np.reshape(label_data,(-1,time_windowLength))
+        print(label_data.shape)
+
+        data_length=len(label_data)
+        
+        if(data_length==0):
+            print('Data not Ready')
+            return
+        
+        number_batch=(int)(data_length/batch_size)
+
+        tmp_mfcc=[]
+        tmp_label=[]
+
+        for i in range((int)(number_batch)):
+            tmp_mfcc.append(mfcc_data[i*batch_size:(i+1)*batch_size])
+            tmp_label.append(label_data[i*batch_size:(i+1)*batch_size])
+            self.test_small_label.data_length+=1
+        self.test_small_label.mfcc_data=tmp_mfcc
+        self.test_small_label.label_data=tmp_label
+        return
+
 
 
 class file(object):
@@ -147,64 +180,66 @@ class RNN_model:
 
     def _build_net(self):
         with tf.variable_scope(self.name):            
-            layer1=35
-            layer2=35
-            layer3=35
+            layer1=32
+            layer2=64
+            layer3=34
             layer4=20
             
             self.layer_input=layer4*2
             self.layer1=40
             self.layer2=10
-            self.layer_output=1
+            self.layer_output=no_output
 
             self.X=tf.placeholder(tf.float32,[None,time_windowLength,vec_per_frame])
-            self.Y_transcripts=tf.placeholder(tf.float32,[None,time_windowLength])
+            self.Y_transcripts=tf.placeholder(tf.int32,[None,time_windowLength])
             self.keep_prob=tf.placeholder(tf.float32)
             self.data_length=tf.placeholder(tf.int32)
+
             
-            layer1_cell=tf.contrib.rnn.BasicLSTMCell(num_units=layer1,activation=tf.nn.tanh)
-            layer4_cell=tf.contrib.rnn.BasicLSTMCell(num_units=layer4,activation=tf.nn.tanh)
             
+            layer1_cell=tf.contrib.rnn.LSTMCell(num_units=layer1,activation=tf.nn.tanh,initializer=tf.contrib.layers.xavier_initializer())
+            layer1_cell=tf.contrib.rnn.DropoutWrapper(layer1_cell,self.keep_prob,self.keep_prob)
+            layer2_cell=tf.contrib.rnn.LSTMCell(num_units=layer2,activation=tf.nn.tanh,initializer=tf.contrib.layers.xavier_initializer())
+            layer3_cell=tf.contrib.rnn.LSTMCell(num_units=layer3,activation=tf.nn.tanh,initializer=tf.contrib.layers.xavier_initializer())
+            layer4_cell=tf.contrib.rnn.LSTMCell(num_units=layer4,activation=tf.nn.tanh,initializer=tf.contrib.layers.xavier_initializer())
+            layer4_cell=tf.contrib.rnn.DropoutWrapper(layer4_cell,self.keep_prob,self.keep_prob)
+
             multi_cell=tf.contrib.rnn.MultiRNNCell([layer1_cell,layer4_cell])
-            
-            rnn_output,_=tf.nn.bidirectional_dynamic_rnn(multi_cell,multi_cell,self.X,dtype=tf.float32)
+
+            layer2_output,_=tf.nn.bidirectional_dynamic_rnn(multi_cell,multi_cell,self.X,dtype=tf.float32)
+            rnn_output=tf.concat(layer2_output,2)
             fc_inputs=tf.reshape(rnn_output,[-1,self.layer_input])
                         
-            W1=tf.Variable(tf.random_normal([self.layer_input,self.layer1]))
+            W1=tf.get_variable('W1',[self.layer_input,self.layer1],tf.float32,tf.contrib.layers.xavier_initializer())
             b1=tf.Variable(tf.random_normal([self.layer1]))
             L1=tf.nn.tanh(tf.matmul(fc_inputs,W1)+b1)
+            L1=tf.nn.dropout(L1,self.keep_prob)
 
-            W2=tf.Variable(tf.random_normal([self.layer1,self.layer2]))
+            W2=tf.get_variable('W2',[self.layer1,self.layer2],tf.float32,tf.contrib.layers.xavier_initializer())
             b2=tf.Variable(tf.random_normal([self.layer2]))
             L2=tf.nn.tanh(tf.matmul(L1,W2)+b2)
+            L2=tf.nn.dropout(L2,self.keep_prob)
 
-            W3=tf.Variable(tf.random_normal([self.layer2,self.layer_output]))
-            b3=tf.Variable(tf.random_normal([self.layer_output]))
-            L3=tf.nn.sigmoid(tf.matmul(L2,W3)+b3)
+            W3=tf.get_variable('W3',[self.layer2,no_output],tf.float32,tf.contrib.layers.xavier_initializer())
+            b3=tf.Variable(tf.random_normal([no_output]))
+            L3=(tf.matmul(L2,W3)+b3)
 
-            self.outputs=tf.reshape(L3,(-1,time_windowLength))           
+            self.outputs=tf.reshape(L3,(-1,time_windowLength,no_output))            
             
             
-        self.cost=tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.outputs,labels=self.Y_transcripts))
+        self.cost=tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.outputs,labels=self.Y_transcripts))
         self.optimizer=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
 
     def show_cost(self,x,y,keep_prob=1.0):
         return self.sess.run(self.cost,feed_dict={self.X:x,self.Y_transcripts:y,self.keep_prob:keep_prob})
-      
+       
     def predict(self,x_test,keep_prob=1.0):
-        out=self.sess.run(self.outputs,feed_dict={self.X:x_test,self.keep_prob:keep_prob})
-        out=np.reshape(out,(-1))
-        for i in range(len(out)):
-            if(out[i]>0.5):
-                out[i]=1
-            else:
-                out[i]=0
-        return out
+        return self.sess.run(tf.argmax(self.outputs,axis=2),feed_dict={self.X:x_test,self.keep_prob:keep_prob})
   
     def output(self,x_test,keep_prob=1.0):
         return self.sess.run(self.outputs,feed_dict={self.X:x_test,self.keep_prob:keep_prob})
 
-    def train(self,x_data,y_data,data_length,keep_prob=1.0):
+    def train(self,x_data,y_data,data_length,keep_prob=0.9):
         return self.sess.run([self.cost,self.outputs,self.optimizer],feed_dict={self.X:x_data,self.Y_transcripts:y_data,self.data_length:data_length,self.keep_prob:keep_prob})
 
     def accuracy(self,x_data,y_data):
